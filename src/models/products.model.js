10c3a307a61b2e27d61dd92124ec7679c6a15161
daviazevdo <azvd.davi@gmail.com>
2022-11-11 18:49:23 -0300

    cria funções do primeiro requisito na camada model

---
+++ src/models/products.model.js
@@ -0,0 +1,24 @@
+const conn = require('./connection');
+
+const findAllProducts = async () => {
+  const [result] = await conn.execute(
+    'SELECT * FROM StoreManager.products',
+  );
+
+  console.log(result);
+
+  return result;
+};
+
+const findProductById = async (productId) => {
+  const [[result]] = await conn.execute(
+    'SELECT * FROM StoreManager.products WHERE id = ?', [productId],
+  );
+
+  return result;
+};
+
+module.exports = {
+  findAllProducts,
+  findProductById,
+};
