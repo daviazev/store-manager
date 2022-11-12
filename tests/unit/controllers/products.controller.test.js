28e3c7ce9ac15f6ec2fdbec1888dea86c7c0f792
daviazevdo <azvd.davi@gmail.com>
2022-11-12 17:26:42 -0300

    adiciona testes para a camada controller

--- tests/unit/controllers/products.controller.test.js
+++ tests/unit/controllers/products.controller.test.js
@@ -60,4 +60,55 @@ describe('Testes de unidade do controller - products', async function () {
     expect(res.status).to.have.been.calledWith(404);
     expect(res.json).to.have.been.calledWith({ message: 'Product not found' })
   })
+
+  it('Retorna o produto criado com sucesso - status 201', async function () {
+    const req = { body: { name: "Product X" } };
+    const res = {};
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon
+      .stub(productsService, 'serviceInsertProduct')
+      .resolves({ type: null, message: 1 });
+    
+    await productsController.controllerInsertProduct(req, res);
+
+    expect(res.status).to.be.calledWith(201);
+  })
+
+  it('Retorna erro - status 400', async function () {
+    const req = { body: { } };
+    const res = {};
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon
+      .stub(productsService, 'serviceInsertProduct')
+      .resolves({ type: '"name" is required', message: '"name" is required' });
+    
+    await productsController.controllerInsertProduct(req, res);
+
+    expect(res.status).to.be.calledWith(400);
+  })
+
+  it('Retorna erro - status 422', async function () {
+    const req = { body: { name: "Prod" } };
+    const res = {};
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon
+      .stub(productsService, 'serviceInsertProduct')
+      .resolves({
+        type: '"name" length must be at least 5 characters long',
+        message: '"name" length must be at least 5 characters long'
+      });
+    
+    await productsController.controllerInsertProduct(req, res);
+
+    expect(res.status).to.be.calledWith(422);
+  })
 });
