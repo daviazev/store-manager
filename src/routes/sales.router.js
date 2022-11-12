b6b9dfb6cceedf7a609a4d034f8166c8d2e546bc
daviazevdo <azvd.davi@gmail.com>
2022-11-12 17:40:24 -0300

    criação da rota sales

---
+++ src/routes/sales.router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+// const salesController = require('../controllers/sales.controller');
+
+const router = express.Router();
+
+router.use(express.json());
+
+module.exports = router;
