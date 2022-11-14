8641743c3025f99f20e5ba4c06353f7e33f69bb8
daviazevdo <azvd.davi@gmail.com>
2022-11-14 15:35:23 -0300

    função que adiciona dados na tabela sales

--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -1,1 +1,18 @@
-// const connection = require('./connection');
+const connection = require('./connection');
+
+const { productsModel } = require('./index');
+
+const dateGnerator = () => new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+const modelInsertSales = async () => {
+  const [{ insertId }] = await connection.execute(
+    'INSERT INTO StoreManager.sales (date) VALUES (?)',
+    [dateGnerator()],
+  );
+
+  return insertId;
+};
+
+module.exports = {
+  modelInsertSales,
+};
