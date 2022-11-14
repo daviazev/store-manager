704e901da6d5384b43dfb846f9958ecdbef37cc6
daviazevdo <azvd.davi@gmail.com>
2022-11-14 16:30:59 -0300

    função que recebe o array de produtos e insere os dados na tabela sales_products

--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -1,6 +1,6 @@
 const connection = require('./connection');
 
-const { productsModel } = require('./index');
+// const { productsModel } = require('./index');
 
 const dateGnerator = () => new Date().toISOString().slice(0, 19).replace('T', ' ');
 
@@ -13,6 +13,18 @@ const modelInsertSales = async () => {
   return insertId;
 };
 
+const modelInsertSalesProducts = async (arrayOfProducts) => {
+  const insertId = await modelInsertSales();
+  
+  arrayOfProducts.map(async (product) => {
+    await connection.execute(
+      'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?)',
+      [insertId, product.productId, product.quantity],
+    );
+  });
+};
+
 module.exports = {
   modelInsertSales,
+  modelInsertSalesProducts,
 };
