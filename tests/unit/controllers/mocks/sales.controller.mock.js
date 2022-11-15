3d787b6807eaeddaa8e6518eeda210ade7f537dc
daviazevdo <azvd.davi@gmail.com>
2022-11-15 18:24:08 -0300

    mocks e testes para a camada controller

--- tests/unit/controllers/mocks/sales.controller.mock.js
+++ tests/unit/controllers/mocks/sales.controller.mock.js
@@ -13,8 +13,44 @@ const saleCreateResponse = {
   ]
 }
 
+const saleId1 = [
+  {
+    date: "2022-11-15T20:35:46.000Z",
+    productId: 1,
+    quantity: 5
+  },
+  {
+    date: "2022-11-15T20:35:46.000Z",
+    productId: 2,
+    quantity: 10
+  }
+]
+
+const allSales = [
+  {
+    saleId: 1,
+    date: "2022-11-15T20:35:46.000Z",
+    productId: 1,
+    quantity: 5
+  },
+  {
+    saleId: 1,
+    date: "2022-11-15T20:35:46.000Z",
+    productId: 2,
+    quantity: 10
+  },
+  {
+    saleId: 2,
+    date: "2022-11-15T20:35:46.000Z",
+    productId: 3,
+    quantity: 15
+  }
+]
+
 module.exports = {
   nonexistentProductIdBody,
   rightSaleBody,
   saleCreateResponse,
+  saleId1,
+  allSales
 }
--- tests/unit/controllers/sales.controller.test.js
+++ tests/unit/controllers/sales.controller.test.js
@@ -10,6 +10,8 @@ const {
   rightSaleBody,
   saleCreateResponse,
   nonexistentProductIdBody,
+  saleId1,
+  allSales
 } = require('./mocks/sales.controller.mock')
 
 const { expect } = chai;
@@ -50,5 +52,56 @@ describe('Testes para a camada service, rota /sales', function () {
 
     expect(res.status).to.have.been.calledWith(404)
     expect(res.json).to.have.been.calledWith({ message: 'Product not found' })
+  })
+
+  it('Retorna todas as vendas', async function () {
+    const req = {};
+    const res = {};
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon
+      .stub(salesService, 'serviceGetAllSales')
+      .resolves({ type: null, message: allSales })
+    
+    await salesController.controllerGetAllSales(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(allSales)
+  })
+
+  it('Retorna a venda correta pelo seu id', async function () {
+    const req = { params: { id: 1 }};
+    const res = {};
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon
+      .stub(salesService, 'serviceGetSaleById')
+      .resolves({ type: null, message: saleId1 })
+    
+    await salesController.controllerGetSaleById(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(saleId1)
+  })
+
+  it('Retorna a venda correta pelo seu id', async function () {
+    const req = { params: { id: 100 }};
+    const res = {};
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon
+      .stub(salesService, 'serviceGetSaleById')
+      .resolves({ message: 'Sale not found' })
+    
+    await salesController.controllerGetSaleById(req, res);
+
+    expect(res.status).to.have.been.calledWith(404);
+    expect(res.json).to.have.been.calledWith({ message: 'Sale not found'})
   })
 });
