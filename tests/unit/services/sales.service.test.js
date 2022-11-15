cf4f2523e68a778c1998412e14202a6f1cd665e2
daviazevdo <azvd.davi@gmail.com>
2022-11-15 18:08:42 -0300

    mocks e testes para a camada service

--- tests/unit/services/sales.service.test.js
+++ tests/unit/services/sales.service.test.js
@@ -5,7 +5,7 @@ const { salesModel } = require('../../../src/models');
 
 const { salesService } = require('../../../src/services');
 
-const { rightSaleBody } = require('./mocks/sales.service.mock');
+const { rightSaleBody, allSales, saleId1 } = require('./mocks/sales.service.mock');
 
 describe('Testes para a camada service, rota /sales', function () {
   afterEach(sinon.restore)
@@ -16,5 +16,29 @@ describe('Testes para a camada service, rota /sales', function () {
     const result = await salesService.serviceInsertSalesProducts(rightSaleBody);
 
     expect(result.insertId).to.be.equal(1);
+  })
+
+  it('Retorna um type null ao buscar por todas as vendas', async function () {
+    sinon.stub(salesModel, 'modelGetAllSales').resolves(allSales);
+
+    const result = await salesService.serviceGetAllSales();
+
+    expect(result.type).to.be.equal(null)
+  })
+
+  it('Retorna um type null ao buscar por todas as vendas', async function () {
+    sinon.stub(salesModel, 'modelGetSaleById').resolves(saleId1);
+
+    const result = await salesService.serviceGetSaleById(1);
+
+    expect(result.type).to.be.equal(null)
+  })
+
+  it('Retorna a mensagem sale not found', async function () {
+    sinon.stub(salesModel, 'modelGetSaleById').resolves([]);
+
+    const { message } = await salesService.serviceGetSaleById(5);
+
+    expect(message).to.be.equal('Sale not found')
   })
 })
