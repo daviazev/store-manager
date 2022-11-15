7595b166789baf7c955a8d1ef44b4fd1c1055547
daviazevdo <azvd.davi@gmail.com>
2022-11-15 17:59:28 -0300

    mocks e testes para a camada model

--- tests/unit/models/sales.model.test.js
+++ tests/unit/models/sales.model.test.js
@@ -6,6 +6,8 @@ const connection = require('../../../src/models/connection');
 
 const {
   rightSaleBody,
+  saleId1,
+  allSales,
 } = require('./mocks/sales.model.mock')
 
 describe('Testes de unidade para a camada model, endpoint /sales', function () {
@@ -17,5 +19,21 @@ describe('Testes de unidade para a camada model, endpoint /sales', function () {
     const result = await salesModel.modelInsertSalesProducts(rightSaleBody)
 
     expect(result).to.be.equal(1);
+  })
+
+  it('retorna todas as vendas com sucesso', async function () {
+    sinon.stub(connection, 'execute').resolves([allSales]);
+
+    const result = await salesModel.modelGetAllSales();
+
+    expect(result).to.be.deep.equal(allSales)
+  })
+
+  it('retorna todas as vendas com sucesso', async function () {
+    sinon.stub(connection, 'execute').resolves([saleId1]);
+
+    const result = await salesModel.modelGetSaleById(1);
+
+    expect(result).to.be.deep.equal(saleId1)
   })
 })
