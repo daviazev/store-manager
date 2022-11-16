a6b4c2d0d10127b689d23ddba525ffa81d364396
daviazevdo <azvd.davi@gmail.com>
2022-11-16 16:42:09 -0300

    testes para a camada model

--- tests/unit/models/products.model.test.js
+++ tests/unit/models/products.model.test.js
@@ -32,4 +32,29 @@ describe('Testes de unidade do model de produtos', () => {
 
     expect(result).to.be.equal(10)
   })
+
+  it('Testa a atualização de produtos', async function () {
+    sinon.stub()
+      .onFirstCall().resolves([[allProductsResponse[0]]])
+      .onSecondCall().resolves(1);
+
+    const product = await productsModel.findProductById(1);
+    const result = await productsModel.modelUpdateProduct('Firebolt', 1);
+
+    expect(product).to.be.deep.equal({ id: 1, name: 'Firebolt' });
+    expect(result).to.be.equal(1);
+  })
+
+  it('Testa se ele não atualiza ao passar um produto com id inexistente', async function () {
+    sinon.stub()
+      .onFirstCall().resolves(undefined)
+      .onSecondCall().resolves(undefined);
+    
+    const product = await productsModel.findProductById(999);
+    const result = await productsModel.modelUpdateProduct('Firebolt', 999);
+
+
+    expect(product).to.be.equal(undefined)
+    expect(result).to.be.equal(undefined)
+  })
 })
